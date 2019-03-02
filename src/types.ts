423a0bd2143b64cdd568cd446aff0edf7800154a
Pavel Koch <kouks.koch@gmail.com>
2019-03-02 16:51:55 +0000

    cleanup

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 import { Request } from '@/contracts/Request'
 
 /**
- * The heart of Awi - the interceptor type used in every .use call. It takes the
- * current state of the request and the users callback edits the contents.
+ * The heart of Awi - the interceptor type used in every `.use` call. It takes
+ * the current state of the request and the user's callback edits its contents.
  */
 export type Interceptor = (request: Request) => Promise<any>
